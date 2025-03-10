27ec51221106a79daf18db68f0376b1c8c14011e
JoseToba <jstb0001@red.ujaen.es>
2025-03-10 08:53:47 +0100

    changes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-
+// Esperar a que el documento esté listo
 $(document).ready(function() {
-    
+    // Evento clic para el botón
     $("#ocultar-btn").click(function() {
-        
+        // Ocultar el párrafo usando el método hide()
         $("#parrafo").hide();
     });
 });
